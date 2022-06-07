d65e35f38172ce6d706779feec52e2cf83164553
nestor <nestorivanmo@comunidad.unam.mx>
2022-06-07 10:24:58 -0500

    added bert predictions

--- bert.py
+++ bert.py
@@ -1,4 +1,39 @@
+import torch
+import pandas as pd
+from transformers import pipeline, AutoTokenizer
+
+transformer_model_name = 'dccuchile/bert-base-spanish-wwm-cased'
+tokenizer = AutoTokenizer.from_pretrained(transformer_model_name)
+transformer_models = ['bondi/bert-semaphore-prediction-w0',
+                      'bondi/bert-semaphore-prediction-w2',
+                      'bondi/bert-semaphore-prediction-w4',
+                      'bondi/bert-semaphore-prediction-w8']
+transformer_models_clean = ['bondi/bert-clean-semaphore-prediction-w0',
+                            'bondi/bert-clean-semaphore-prediction-w2',
+                            'bondi/bert-clean-semaphore-prediction-w4',
+                            'bondi/bert-clean-semaphore-prediction-w8']
+
+semaphore_classes = ['amarillo', 'naranja', 'rojo', 'verde']
+semaphore_labels = ['LABEL_1', 'LABEL_2', 'LABEL_3', 'LABEL_0']
+mapping = dict(zip(semaphore_labels, semaphore_classes))
+
+def get_prediction_vector(text, clean_models=False):
+    transformer_pipelines = []
+    tokenizer_kwargs = {'padding': True, 'truncation': True, 'max_length': 512}
+    if clean_models:
+        for model in transformer_models_clean:
+            transformer_pipelines.append(pipeline(model=model, tokenizer=tokenizer))
+    else:
+        for model in transformer_models:
+            transformer_pipelines.append(pipeline(model=model, tokenizer=tokenizer))
 
 
-def get_prediction(data):
-    return 1
+    def predict(text, pipeline):
+        return pipeline(text, **tokenizer_kwargs)[0]['label']
+
+
+    label_dict = {'W0': None, 'W2': None, 'W4': None, 'W8': None}
+    label_vector = [None] * len(transformer_pipelines)
+    for i in range(len(transformer_pipelines)):
+        label_vector[i] = mapping[predict(text, transformer_pipelines[i])]
+    return label_vector
--- timeseries_utils.py
+++ timeseries_utils.py
@@ -1,15 +1,19 @@
 import pandas as pd
 from datetime import datetime, timedelta
 
-def generate_dataframe():
-    zero_week = datetime.now()
+def generate_dataframe(date, prediction):
+    if date is None:
+        zero_week = datetime.now()
+    else:
+        date_str = date[8:10] + date[5:7] + date[2:4]
+        zero_week = datetime.strptime(date_str, '%d%m%y')
     two_weeks = zero_week + timedelta(days=14)
     four_weeks = zero_week + timedelta(days=30)
     eight_weeks = zero_week + timedelta(days=60)
 
     df = pd.DataFrame({
         'date' : [zero_week, two_weeks, four_weeks, eight_weeks],
-        'semaphore' : ['Green', 'Yellow', 'Orange', 'Red']
+        'semaphore' : prediction
     })
     df['date'] = pd.to_datetime(df['date'])
     df.set_index('date', inplace=True)
